2c47972c0bf222ebd18e666348fc90372142122c
hyeji Kim <hyexkim@gmail.com>
2020-03-03 22:33:04 +0900

    Create 1008.py

---
+++ BOJ/python3/1008.py
@@ -0,0 +1,26 @@
+"""
+문제
+두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+"""
+
+# v1
+a,b = input().split()
+a = float(a)
+b = float(b)
+print(a/b)
+
+# v2
+a,b = input().split()
+a = float(a)
+b = float(b)
+print(round(a/b,9))
+
+# v3
+a,b= map(int, input().split())
+print('%0.9f'%(a/b))
