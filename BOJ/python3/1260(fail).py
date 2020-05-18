52fc1d0819d84d5b05146b15ef3af551063e5c0a
hyeji Kim <hyexkim@gmail.com>
2020-05-19 02:52:21 +0900

    Update 1260(fail).py

--- BOJ/python3/1260(fail).py
+++ BOJ/python3/1260(fail).py
@@ -1,8 +1,28 @@
 from queue import Queue
 from collections import defaultdict
 
+
+def dfs(graph, start_node):
+    visited = [0 for _ in range(n+1)]
+    stack = list()
+    result = []
+
+    stack.append(start_node)
+
+    while stack:
+        node = stack.pop()
+        if visited[node] == 0:
+            visited[node] = 1
+            result.append(node)
+            stack.extend(x for x in reversed(graph[node]) if x not in result)
+
+    return result
+
+
 def bfs(graph, start_node):
-    visit = {}
+    visited = [0 for _ in range(n + 1)]
+    result = []
+    # visit = {}
     # visit = list()
     queue = Queue()
 
@@ -10,30 +30,13 @@ def bfs(graph, start_node):
 
     while queue.qsize() > 0:
         node = queue.get()
-        if node not in visit:
-            visit[node] = True
-            # visit.append(node)
+        if visited[node] == 0:
+            visited[node] = 1
+            result.append(node)
             for nextNode in graph[node]:
-                queue.put(nextNode)
-    return list(visit.keys())
-    # return visit
-
-
-def dfs(graph, start_node):
-    visit = {}
-    # visit = list()
-    stack = list()
-
-    stack.append(start_node)
-
-    while stack:
-        node = stack.pop()
-        if node not in visit:
-            visit[node] = True
-            # visit.append(node)
-            stack.extend(reversed(graph[node]))
-    return list(visit.keys())
-    # return visit
+                if nextNode not in result:
+                    queue.put(nextNode)
+    return result
 
 
 n, m, v = map(int, input().split())
@@ -47,15 +50,41 @@ for i in range(m):
 for key, value in inp.items():
     inp[key] = sorted(value)
 
-d = dfs(inp, v)
-b = bfs(inp, v)
-
-for i in range(len(d)):
-    print(d[i], end=" ")
-print("")
-
-for i in range(len(b)):
-    print(b[i], end=" ")
-print("")
+print(*dfs(inp, v))
+print(*bfs(inp, v))
 
 
+
+# 1260 에서 먹힌 코드
+# 내 코드(위)가 안되는 이유는 아직도 모르겠다
+
+# def dfs(v):
+#     print(v, end=' ')
+#     visit[v] = 1
+#     for i in range(1, n+1):
+#         if visit[i] == 0 and s[v][i] == 1:
+#             dfs(i)
+
+# def bfs(v):
+#     queue = [v]
+#     visit[v] = 0
+#     while(queue):
+#         v = queue[0]
+#         print(v, end=' ')
+#         del queue[0]
+#         for i in range(1, n+1):
+#             if visit[i] == 1 and s[v][i] == 1:
+#                 queue.append(i)
+#                 visit[i] = 0
+
+# n, m, v = map(int, input().split())
+# s = [[0] * (n + 1) for i in range(n+1)]
+# visit = [0 for i in range(n + 1)]
+# for i in range(m):
+#     x,y = map(int, input().split())
+#     s[x][y] = 1
+#     s[y][x] = 1
+
+# dfs(v)
+# print()
+# bfs(v)
