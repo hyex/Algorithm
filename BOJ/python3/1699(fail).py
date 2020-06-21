cb13d2676715238ad0446dbbbac6e831a974ee09
hyeji Kim <hyexkim@gmail.com>
2020-06-21 19:59:47 +0900

    Update 1699(fail).py

--- BOJ/python3/1699(fail).py
+++ BOJ/python3/1699(fail).py
@@ -1,23 +1,12 @@
 N = int(input())
-arr = [100000] * (N+1)
-square = []
-
-for i in range(N+1):
-    x = i*i 
-    if x > N:
-        break
-    square.append(x)
-    arr[x] = 1
-
-# print(arr)
-
-for idx in range(N+1):
-    for sq in square:
-        # print("idx = ", idx, "sq = ", sq)
-        if idx < sq:
+dp = [0 for i in range(N+1)]
+square = [i*i for i in range(1, 317)]
+for i in range(1, N+1):
+    s = []
+    for j in square:
+        if j > i:
             break
-        # print("arr[idx] = ", arr[idx], " arr[sq] + arr[idx-sq] = ", arr[sq] + arr[idx-sq])
-        arr[idx] = min(arr[idx], arr[sq] + arr[idx-sq])
-        # print(arr[idx])
+        s.append(dp[i - j])
+    dp[i] = min(s) + 1
 
-print(arr[N])
+print(dp[N])
